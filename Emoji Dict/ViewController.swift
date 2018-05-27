0e65520c1e70a3d2485bab3f51aa1c0d6c6c5f80
Vismayak Mohanarajan <vismayak97@gmail.com>
2018-05-27 16:58:46 -0500

    Created TableView

--- Emoji Dict/ViewController.swift
+++ Emoji Dict/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet weak var MainTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        MainTableView.dataSource = self;
+        MainTableView.delegate = self;
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let cell = UITableViewCell() // creating a generic table view cell
+        cell.textLabel?.text = "Test"; 
+        // "“When working with optional values, you can write ? before
+        //operations like methods, properties, and subscripting. If the value before the ? is nil,
+        //everything after the ? is ignored and the value of the whole expression is nil. Otherwise, the
+        //optional value is unwrapped, and everything after the ? acts on the unwrapped value.”
+        
+        return cell
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
