9e33cc44bc98e1fce88717de1fbeea1cbfb085a4
Vismayak Mohanarajan <vismayak97@gmail.com>
2018-05-27 16:19:34 -0500

    Initial Commit

---
+++ Emoji Dict/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dict
+//
+//  Created by Vismayak Mohanarajan on 5/27/18.
+//  Copyright © 2018 Vismayak Mohanarajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
